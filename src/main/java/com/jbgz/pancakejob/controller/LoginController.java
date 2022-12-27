2b4be1c35a4cb8b75101d684534a08d287341682
Lmq2002 <1657996876@qq.com>
2022-12-28 05:41:38 +0800

    登录注册子系统三个API的开发，初步放置。

--- src/main/java/com/jbgz/pancakejob/controller/LoginController.java
+++ src/main/java/com/jbgz/pancakejob/controller/LoginController.java
@@ -14,21 +14,62 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "hello springboot";
-    }
-
-    @PostMapping("/find")
-    public int find(@RequestBody User user){
-        //仅供参考
-        return userService.find(user);
-    }
-
-    @GetMapping("/page")
-    public List<User> findPage(@RequestParam Integer pageNum,@RequestParam Integer pageSize)
+    /**
+     * 功能：注册用户
+     * 状态：正在开发中
+     * 负责人:Lmq
+     * 新建时间：2022/12/28
+     * */
+    @PostMapping("/register")
+    public boolean regist(@RequestBody User user)
     {
-        // 仅供参考，如何获取参数
-        return null;
+        userService.save(user);
+        return true;
     }
+
+    /**
+     * 功能：登录，记录conlog
+     * 状态：正在开发中
+     * 负责人：Lmq
+     * 新建时间：2022/12/28
+     * */
+    @PostMapping("/login")
+    public boolean login(@RequestBody User user)
+    {
+        if(userService.find(user)==1)
+        {
+            //...
+            //conlogService.save()
+        }
+        return true;
+    }
+
+    /**
+     * 功能：修改密码
+     * 状态：正在开发中
+     * 负责人：Lmq
+     * 新建时间：2022/12/28
+     * */
+    @PutMapping("/password")
+    public boolean modifyPassword(@RequestBody User user)
+    {
+        return true;
+    }
+//    @RequestMapping("/hello")
+//    public String hello() {
+//        return "hello springboot";
+//    }
+//
+//    @PostMapping("/find")
+//    public int find(@RequestBody User user){
+//        //仅供参考
+//        return userService.find(user);
+//    }
+//
+//    @GetMapping("/page")
+//    public List<User> findPage(@RequestParam Integer pageNum,@RequestParam Integer pageSize)
+//    {
+//        // 仅供参考，如何获取参数
+//        return null;
+//    }
 }
